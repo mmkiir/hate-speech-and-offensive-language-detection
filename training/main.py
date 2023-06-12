4636268694aaad84ae80a9f245750d3fd510c6a8
Porasjeet Singh <porasjeet@gmail.com>
2023-06-12 18:16:11 +0530

    feat: create CNN model

--- training/main.py
+++ training/main.py
@@ -5,13 +5,19 @@ import nltk
 import numpy as np
 import pandas as pd
 from keras.callbacks import EarlyStopping
-from keras.layers import LSTM, Dense, Embedding
+from keras.layers import (
+    LSTM,
+    Bidirectional,
+    Conv1D,
+    Dense,
+    Embedding,
+    GlobalMaxPooling1D,
+)
 from keras.models import Sequential
 from keras.preprocessing.text import Tokenizer
 from keras.utils import pad_sequences, to_categorical
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
-from keras.layers import Bidirectional
 
 nltk.download("stopwords")
 
@@ -67,7 +73,7 @@ def preprocess_data(data):
     return text, labels, vocab_size
 
 
-def create_model_2(vocab_size, max_length, num_classes):
+def create_lstm(vocab_size, max_length, num_classes):
     model = Sequential()
     model.add(Embedding(input_dim=vocab_size, output_dim=32, input_length=max_length))
     model.add(LSTM(128, return_sequences=True))
@@ -80,7 +86,7 @@ def create_model_2(vocab_size, max_length, num_classes):
     return model
 
 
-def train_model_2(model, X_train, y_train, X_test, y_test):
+def train_lstm(model, X_train, y_train, X_test, y_test):
     es = EarlyStopping(monitor="val_loss", mode="min", verbose=1, patience=3)
     model.summary()
     model.fit(
@@ -93,7 +99,7 @@ def train_model_2(model, X_train, y_train, X_test, y_test):
     )
 
 
-def create_model_3(vocab_size, max_length, num_classes):
+def create_bilstm(vocab_size, max_length, num_classes):
     model = Sequential()
     model.add(Embedding(input_dim=vocab_size, output_dim=32, input_length=max_length))
     model.add(Bidirectional(LSTM(128, return_sequences=True)))
@@ -106,7 +112,33 @@ def create_model_3(vocab_size, max_length, num_classes):
     return model
 
 
-def train_model_3(model, X_train, y_train, X_test, y_test):
+def train_bilstm(model, X_train, y_train, X_test, y_test):
+    es = EarlyStopping(monitor="val_loss", mode="min", verbose=1, patience=3)
+    model.summary()
+    model.fit(
+        X_train,
+        y_train,
+        batch_size=1024,
+        epochs=10,
+        validation_data=(X_test, y_test),
+        callbacks=[es],
+    )
+
+
+def create_cnn(vocab_size, max_length, num_classes):
+    model = Sequential()
+    model.add(Embedding(input_dim=vocab_size, output_dim=32, input_length=max_length))
+    model.add(Conv1D(128, 5, activation="relu"))
+    model.add(GlobalMaxPooling1D())
+    model.add(Dense(32, activation="relu"))
+    model.add(Dense(num_classes, activation="softmax"))
+    model.compile(
+        loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"]
+    )
+    return model
+
+
+def train_cnn(model, X_train, y_train, X_test, y_test):
     es = EarlyStopping(monitor="val_loss", mode="min", verbose=1, patience=3)
     model.summary()
     model.fit(
@@ -135,24 +167,37 @@ y_train = np.array(y_train)
 y_test = np.array(y_test)
 
 num_classes = len(set(labels))
-y_train_categorical = to_categorical(y_train, num_classes=num_classes)
-y_test_categorical = to_categorical(y_test, num_classes=num_classes)
-y_train_categorical = np.array(y_train_categorical)
-y_test_categorical = np.array(y_test_categorical)
+y_train_cat = to_categorical(y_train, num_classes=num_classes)
+y_train_cat = np.array(y_train_cat)
+y_test_cat = to_categorical(y_test, num_classes=num_classes)
+y_test_cat = np.array(y_test_cat)
 
 # Create and train model 1 (Naive Bayes)
-model_1 = MultinomialNB()
-model_1.fit(X_train, y_train)
+naive_bayes = MultinomialNB()
+naive_bayes.fit(X_train, y_train)
 
 # Create and train model 2 (LSTM)
-model_2 = create_model_2(vocab_size, max_length=150, num_classes=num_classes)
-train_model_2(model_2, X_train, y_train_categorical, X_test, y_test_categorical)
+lstm = create_lstm(vocab_size, max_length=150, num_classes=num_classes)
+train_lstm(lstm, X_train, y_train_cat, X_test, y_test_cat)
 
 # Create and train model 3 (BiLSTM)
-model_3 = create_model_3(vocab_size, max_length=150, num_classes=num_classes)
-train_model_3(model_3, X_train, y_train_categorical, X_test, y_test_categorical)
+bilstm = create_bilstm(vocab_size, max_length=150, num_classes=num_classes)
+train_bilstm(bilstm, X_train, y_train_cat, X_test, y_test_cat)
+
+# Create and train model 4 (CNN)
+cnn = create_cnn(vocab_size, max_length=150, num_classes=num_classes)
+X_train_cnn = np.expand_dims(X_train, axis=2)
+X_test_cnn = np.expand_dims(X_test, axis=2)
+train_cnn(
+    cnn,
+    X_train_cnn,
+    y_train_cat,
+    X_test_cnn,
+    y_test_cat,
+)
 
 # Get the accuracy of all models
-print(f"Model 1 (Naive Bayes) accuracy: {model_1.score(X_test, y_test)}")
-print(f"Model 2 (LSTM) accuracy: {model_2.evaluate(X_test, y_test_categorical)[1]}")
-print(f"Model 3 (BiLSTM) accuracy: {model_3.evaluate(X_test, y_test_categorical)[1]}")
+print(f"Naive Baye accuracy: {naive_bayes.score(X_test, y_test)}")
+print(f"CNN accuracy: {cnn.evaluate(X_test, y_test_cat)[1]}")
+print(f"LSTM accuracy: {lstm.evaluate(X_test, y_test_cat)[1]}")
+print(f"BiLSTM accuracy: {bilstm.evaluate(X_test, y_test_cat)[1]}")
